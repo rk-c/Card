33b04f6a299c7c6a32a3a6c50e75c774c0e6783d
Roman <roman.kozlov@inbox.ru>
2020-04-01 14:44:08 +0300

    Initial Commit

---
+++ Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Card
+//
+//  Created by Рома on 01.04.2020.
+//  Copyright © 2020 Рома. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
